f2aaeeffb471a84501e5e6ca0eaa834ac4d19d8d
IvanCaravan <ivan.kirsanov.2000@mail.ru>
2023-02-10 00:07:17 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String city = "Vologda"; /* As you can imagine, this city means a lot
+        for
+        me*/
+        System.out.println(city); // and i want to share its name with you!
+
+
     }
 }
